78f5912bb18144cdde054d67a0192f77a3b45b3a
leandrososa <52506861+leandrososa@users.noreply.github.com>
2020-11-09 18:56:17 -0300

    Created combobox component based on Vuetify combobox

---
+++ src/components/form/Combobox.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-combobox style="font-weight:600" outlined :label="label"></v-combobox>
+</template>
+<script>
+    export default {
+        name: 'Combobox',
+        props: {
+            'label': {
+                default: ''
+            }
+        }
+    }
+</script>
