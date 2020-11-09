6acea234582402023e76c9de1d598b12c3fd485e
leandrososa <52506861+leandrososa@users.noreply.github.com>
2020-11-09 18:56:59 -0300

    Created textinput component base on Vuetify textfield

---
+++ src/components/form/TextInput.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-text-field style="font-weight:600" :label="label" :outlined="outlined" :single-line="singleline"></v-text-field>
+</template>
+<script>
+    export default {
+        name: 'TextInput',
+        props: {
+            'label': {
+                default: ''
+            },
+            'singleline': {
+                type: Boolean,
+                default: false
+            },
+            'outlined': {
+                type: Boolean,
+                default: true
+            }
+        }
+    }
+</script>
