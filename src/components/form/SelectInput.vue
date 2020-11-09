6b76c3689a3f4aad3337947672cee248d4d31354
leandrososa <52506861+leandrososa@users.noreply.github.com>
2020-11-09 18:56:38 -0300

    Created selectinput component based on Vuetify select

---
+++ src/components/form/SelectInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-select style="font-weight:600" outlined :label="label"></v-select>
+</template>
+<script>
+    export default {
+        name: 'SelectInput',
+        props: {
+            'label': {
+                default: ''
+            }
+        }
+    }
+</script>
