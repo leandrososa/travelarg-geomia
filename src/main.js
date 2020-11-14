b9bd324b6c7b07e92fc827b8476c78d084df0fcd
leandrososa <52506861+leandrososa@users.noreply.github.com>
2020-11-14 01:50:55 -0300

    Added prototype and session storage package

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import VueSessionStorage from 'vue-sessionstorage';
 
+Vue.use(VueSessionStorage); // initialize  vue session storage
 Vue.config.productionTip = false;
+Vue.prototype.$windowReload = function(){ // function in prototype to access window js object
+  window.location.reload();
+} 
 
 new Vue({
   router,
