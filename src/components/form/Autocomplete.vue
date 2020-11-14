360a75ad77bd31bfba2652bf1f7073f6743bb581
leandrososa <52506861+leandrososa@users.noreply.github.com>
2020-11-14 01:52:54 -0300

    New autocomplete component

---
+++ src/components/form/Autocomplete.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-autocomplete style="font-weight:600" outlined :label="label" :rounded="rounded" :dense="dense" :append-icon="appendIcon" :item-text="itemText"></v-autocomplete>
+</template>
+<script>
+    export default {
+        name: 'Autocomplete',
+        props: {
+            'label': {
+                default: ''
+            },
+            'rounded': {
+                type: Boolean,
+                default: false
+            },
+            'dense': {
+                type: Boolean,
+                default: false
+            },
+            'appendIcon': {
+                default: ''
+            },
+            'itemText': {
+                default: ''
+            }
+        }
+    }
+</script>
