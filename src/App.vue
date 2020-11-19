8a40d88324c539765b6d049096c99579a81f5960
leandrososa <52506861+leandrososa@users.noreply.github.com>
2020-11-19 02:48:31 -0300

    Unused comment remove

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,5 @@
 <template>
   <v-app>
-    <!-- <v-app-bar app color="white" height="115px" dark>
-      <v-container>
-        <v-row align="center">
-          <div class="d-flex align-center">
-            <router-link to="/">
-              <v-img
-                alt="TravelArg"
-                contain
-                src="@/assets/logo-travelarg.svg"
-                width="200"
-                height="50"
-              />  
-            </router-link>
-          </div>
-          <v-spacer></v-spacer>
-          <v-btn to="/destinos" text color="secondary">
-            <span>Destinos</span>
-          </v-btn>
-
-          <v-btn to="/articulos" text color="secondary">
-            <span>Artículos</span>
-          </v-btn>
-
-          <v-btn to="/travelers" text color="secondary">
-            <span>Travelers</span>
-          </v-btn>
-
-          <v-btn to="/livestreams" text color="secondary">
-            <span>Livestreams</span>
-          </v-btn>
-
-          <v-btn to="/reservas" text color="secondary">
-            <span>Reservas</span>
-          </v-btn>
-
-          <v-avatar class="ml-4 mr-4" color="primary"><span class="white--text headline">LS</span></v-avatar>
-
-          <v-btn
-            target="_blank"
-            color="primary"
-            elevation="0"
-            rounded
-          >
-            <v-icon>mdi-pencil</v-icon>
-            <span class="ml-2">Escribir reseña</span>
-            
-          </v-btn>
-        </v-row>
-      </v-container>
-    </v-app-bar> -->
     <Navigation></Navigation>
     <v-main>
       <GeomiaHeader></GeomiaHeader>
